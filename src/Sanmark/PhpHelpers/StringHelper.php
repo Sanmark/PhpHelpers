e19817ca8d2800ea45ee8159f0f344b5cc5604ee
Supun Budhajeewa <budhajeewa@thesanmark.com>
2015-08-03 15:22:56 +0530

    CM StringHelper::genRandom().

--- src/Sanmark/PhpHelpers/StringHelper.php
+++ src/Sanmark/PhpHelpers/StringHelper.php
@@ -14,4 +14,20 @@ namespace Sanmark\PhpHelpers
 			return $spacesRemovedString ;
 		}
 	}
+
+	public static function genRandom ( $length = 10 )
+		{
+			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' ;
+
+			$charactersLength = strlen ( $characters ) ;
+
+			$randomString = '' ;
+
+			for ( $i = 0 ; $i < $length ; $i ++ )
+			{
+				$randomString .= $characters[ rand ( 0 , $charactersLength - 1 ) ] ;
+			}
+
+			return $randomString ;
+		}
 }
